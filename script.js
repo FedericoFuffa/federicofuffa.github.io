7be346c8e22d63269e0801a7955321d4883b81a6
FedericoFuffa <88799248+FedericoFuffa@users.noreply.github.com>
2021-08-16 03:23:09 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const showMenu = document.getElementById('nav-toggle'),
-    sidebar = document.getElementById('sidebar') 
-
-    if(sidebar && showMenu){
-        showMenu.addEventListener('click',()=>{
-            sidebar.classList.toggle('show')
-            showMenu.classList.toggle('rotate')
-        })
-    }
